fdda2cfc45806635f3b883919416b09798631af5
Tyler jarrard <tjar2074@740s-pl-104.local>
2016-02-17 09:56:59 -0700

    saving

--- src/view/Panel2d.java
+++ src/view/Panel2d.java
@@ -1,9 +1,12 @@
 package view;
 
 import java.awt.Color;
+
 import javax.swing.*;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
@@ -20,9 +23,9 @@ public class Panel2d extends JPanel
 	private JTextField firstTextField;
 	private SpringLayout baseLayout;
 	private JTextField typingField;
-	private JLabel promptLabel;
-	private JTextArea chatArea;
-	private JCheckBox check;
+	private JTextField nRow;
+	private JTextArea entityArea;
+	private JTextField nCol;
 	
 	public Panel2d(Controller2d baseController)
 	{
@@ -30,13 +33,13 @@ public class Panel2d extends JPanel
 		baseLayout = new SpringLayout();
 		submitButton = new JButton("click the button");
 		firstTextField = new JTextField("words can be type here", 20);
-		chatArea = new JTextArea(10,30);
-		promptLabel = new JLabel("Chat with me");
+		entityArea = new JTextArea(10,30);
 		typingField = new JTextField("asda");
-		check = new JCheckBox("Orange", false);
+		nRow = new JTextField("8");
+		nCol = new JTextField("8");
 		
 		setupPanel();
-//		setupLayout();
+		setupLayout();
 		setupListeners();
 	}
 	
@@ -45,19 +48,42 @@ public class Panel2d extends JPanel
 		this.setLayout(baseLayout);
 		this.setBackground(Color.MAGENTA);
 		this.add(firstTextField);
-		this.add(chatArea);
+		this.add(nRow);
+		this.add(nCol);
+		this.add(entityArea);
 		this.add(submitButton);
-		this.add(check);
-		this.add(promptLabel);
 		typingField.setToolTipText("Type here for the chatbot");
-		chatArea.setEnabled(false);
-		
+		entityArea.setEnabled(false);
+	}
+	
+	private void setupLayout()
+	{
+		baseLayout.putConstraint(SpringLayout.SOUTH, firstTextField, -25, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, submitButton, 0, SpringLayout.WEST, firstTextField);
+		baseLayout.putConstraint(SpringLayout.SOUTH, submitButton, -2, SpringLayout.NORTH, firstTextField);
+		baseLayout.putConstraint(SpringLayout.WEST, firstTextField, 10, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, entityArea, -103, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, entityArea, -40, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, nRow, 0, SpringLayout.NORTH, firstTextField);
+		baseLayout.putConstraint(SpringLayout.WEST, nRow, 6, SpringLayout.EAST, firstTextField);
+		baseLayout.putConstraint(SpringLayout.NORTH, nCol, 0, SpringLayout.NORTH, firstTextField);
+		baseLayout.putConstraint(SpringLayout.WEST, nCol, 6, SpringLayout.EAST, nRow);
 		
 	}
 	
 	private void setupListeners()
 	{
-	
+		submitButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				int row = nRow.getText();
+				
+				String userText = firstTextField.getText(); 
+				firstTextField.setText("");
+//				entityArea.append(String[][] entity); 
+			}
+		});
 	}
 	
 }
